60a4f90a0ad2405f8c610094e3d2c4e44456b015
zcd06 <zcd.06@outlook.com>
2020-07-16 12:03:51 -0400

    Make component recipe-detail work using event and property binding

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -8,9 +8,11 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Array<Recipe> = [
-    new Recipe('Test Recipe', 'This is a test recipe', 'https://liveitforward.com/wp-content/uploads/2018/05/food-layout_edited_123rf_-53694353_m.png'),
-    new Recipe('Test Recipe', 'This is a test recipe', 'https://liveitforward.com/wp-content/uploads/2018/05/food-layout_edited_123rf_-53694353_m.png')
+    new Recipe('Test Recipe', 'This is a test recipe', 'https://equalexchange.coop/sites/default/files/styles/page_header_image__normal/public/recipe/recipe-fav-choc-chip-cookie_0.jpg?itok=WXWMiVIT'),
+    new Recipe('Another Test Recipe', 'This is an another test recipe', 'http://i.ytimg.com/vi/jNB_-NoN-bU/maxresdefault.jpg')
   ];
+
+  @Output() recipeSelected = new EventEmitter<Recipe>();
 
   constructor() { }
 
