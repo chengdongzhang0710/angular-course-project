3af1f88854866c2c0a67f241148c7b9d4d86e591
zcd06 <zcd.06@outlook.com>
2020-07-16 13:32:17 -0400

    Make component shopping-list add function work

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Output, EventEmitter } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
+  @ViewChild('nameInput', {static: false}) ingredientName: ElementRef;
+  @ViewChild('amountInput', {static: false}) ingredientAmount: ElementRef;
+  @Output() ingredientAdded = new EventEmitter<Ingredient>()
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onAddIngredient() {
+    this.ingredientAdded.emit({name: this.ingredientName.nativeElement.value, amount: this.ingredientAmount.nativeElement.value})
   }
 
 }
