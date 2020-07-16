c08c54b724b4f65647d9247f8638711e9bdf182f
zcd06 <zcd.06@outlook.com>
2020-07-16 16:58:11 -0400

    Make dropdown menu in the application work

---
+++ src/app/shared/dropdown.directive.ts
@@ -0,0 +1,28 @@
+import { Directive, HostListener, Renderer2, ElementRef, HostBinding } from '@angular/core';
+
+@Directive({
+  selector: '[appDropdown]'
+})
+export class DropdownDirective {
+  // dropdownOpened = false;
+
+  // constructor(private elementRef: ElementRef, private renderer: Renderer2) { }
+
+  // @HostListener('click') openDropdown() {
+  //   if (!this.dropdownOpened) {
+  //     this.renderer.addClass(this.elementRef.nativeElement, 'open');
+  //     this.dropdownOpened = true;
+  //   } else {
+  //     this.renderer.removeClass(this.elementRef.nativeElement, 'open');
+  //     this.dropdownOpened = false;
+  //   }
+  // }
+
+  @HostBinding('class.open') dropOpened = false;
+  @HostListener('document:click', ['$event']) openDropdown(event: Event) {
+    this.dropOpened = this.elRef.nativeElement.contains(event.target) ? !this.dropOpened : false;
+  }
+
+  constructor(private elRef: ElementRef) { }
+
+}
