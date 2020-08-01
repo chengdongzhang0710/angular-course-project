880432d22aa6bc188ec05ac81b6d66707690df38
zcd06 <zcd.06@outlook.com>
2020-08-01 10:58:17 -0400

    Add feature module recipes, shopping-list and auth, shared module and core module to the application

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { AuthComponent } from './auth.component';
+import { SharedModule } from '../shared/shared.module';
+
+const routes: Routes = [{ path: 'auth', component: AuthComponent }];
+
+@NgModule({
+  declarations: [AuthComponent],
+  imports: [RouterModule.forChild(routes), FormsModule, SharedModule],
+})
+export class AuthModule {}
